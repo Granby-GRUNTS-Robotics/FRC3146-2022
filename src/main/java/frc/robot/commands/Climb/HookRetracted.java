760e473801f86b26613be42ebda19328f0459135
Prior-W <priordoubleu@gmail.com>
2022-02-03 19:53:05 -0500

    Added commands for each hook enum

---
+++ src/main/java/frc/robot/commands/Climb/HookRetracted.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Climb;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.PneumaticConstants.HOOK_ENUM;
+import frc.robot.subsystems.Climb;
+
+public class HookRetracted extends CommandBase {
+  /** Creates a new HookRetracted. */
+  Climb climb;
+  public HookRetracted(Climb climb) {
+    this.climb=climb;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(climb);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    climb.setHook(HOOK_ENUM.RETRACTED);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
