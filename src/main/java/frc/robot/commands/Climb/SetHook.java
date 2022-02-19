4d2dcdcf6fb8b7b876e19a9246e03508e8921cd9
Prior-W <46361128+Prior-W@users.noreply.github.com>
2022-02-18 23:27:46 -0500

    Update SetHook.java
    Added all of the meat and potatoes for this class

--- src/main/java/frc/robot/commands/Climb/SetHook.java
+++ src/main/java/frc/robot/commands/Climb/SetHook.java
@@ -10,13 +10,27 @@ import frc.robot.subsystems.Climb;
 
 public class SetHook extends CommandBase {
   /** Creates a new SetHook. */
+  private Climb climb;
+  private HOOK_ENUM state;
+  private boolean ends;
   public SetHook(Climb climb,HOOK_ENUM state) {
+    this.climb = climb;
+    this.state = state;
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  public SetHook(Climb climb,HOOK_ENUM state, boolean ends) {
+    this.climb = climb;
+    this.state = state;
+    this.ends = ends;
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    climb.setHook(state);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -24,11 +38,14 @@ public class SetHook extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    if(ends == true)
+    climb.setClimbPercent(0);
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return climb.isAtPosition();
   }
 }
