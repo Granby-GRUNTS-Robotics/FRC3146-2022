a96424bd029663658bf51d76ac44e7736a732cdc
Noah Collen <noahcollen@gmail.com>
2022-03-26 13:50:20 -0400

    Magazine and Intake
    Intake runs for all shooting settings along with magazine

--- src/main/java/frc/robot/commands/Shooter/ShootBase.java
+++ src/main/java/frc/robot/commands/Shooter/ShootBase.java
@@ -5,23 +5,25 @@
 package frc.robot.commands.Shooter;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.MagazineAndIntakeFeed;
 import frc.robot.commands.Magazine.BackSpace;
 import frc.robot.commands.Magazine.Feed;
 import frc.robot.commands.Magazine.FeedAuto;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Magazine;
 import frc.robot.subsystems.Shooter;
 
 /**base for shooting. Backspaces, revs up(the thing that will change), feeds, and then brakes the shooter (except it doesn't really do that, because the shooter motor is in coast mode) */
 public class ShootBase extends SequentialCommandGroup {
   /** Creates a new ShootBase. */
-  public ShootBase(Magazine magazine, Shooter shooter, RevUpBase speedBase) {
+  public ShootBase(Magazine magazine, Shooter shooter, RevUpBase speedBase, Intake intake) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new BackSpace(magazine), speedBase, new Feed(magazine), new ShooterBrake(shooter));
+    addCommands(new BackSpace(magazine), speedBase, new MagazineAndIntakeFeed(intake, magazine), new ShooterBrake(shooter));
   }
-  public ShootBase(Magazine magazine, Shooter shooter, RevUpBase speedBase, boolean auto) {
+  public ShootBase(Magazine magazine, Shooter shooter, RevUpBase speedBase, boolean auto, Intake intake) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new BackSpace(magazine), speedBase, auto ? new FeedAuto(magazine): new Feed(magazine), new ShooterBrake(shooter));
+    addCommands(new BackSpace(magazine), speedBase, auto ? new FeedAuto(magazine): new MagazineAndIntakeFeed(intake, magazine), new ShooterBrake(shooter));
   }
 }
