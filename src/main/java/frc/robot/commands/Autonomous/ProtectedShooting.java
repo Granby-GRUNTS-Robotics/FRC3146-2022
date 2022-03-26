eedf3451525181b8147bd8e112553398307b3ae1
eekanshM <78319535+eekanshM@users.noreply.github.com>
2022-03-26 13:55:12 -0400

    Added protected shooting routine

---
+++ src/main/java/frc/robot/commands/Autonomous/ProtectedShooting.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Autonomous;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.commands.Drivetrain.DriveToAngle;
+import frc.robot.commands.Drivetrain.DriveToLocation;
+import frc.robot.commands.Shooter.ShootWithSetSpeed;
+import frc.robot.subsystems.Drivetrain;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class ProtectedShooting extends SequentialCommandGroup {
+
+
+  /** Creates a new ProtectedShooting. */
+  public ProtectedShooting(Magazine magazine, Intake intake, Drivetrain drivetrain, Shooter shooter) {
+    
+    //Drive to location allows the robot to move a given distance
+    addCommands(new DriveToLocation(drivetrain, 58.75), new DriveToAngle(drivetrain, Constants.SetpointConstants.PROTECTED_SHOOTING_ANGLE), new ShootWithSetSpeed(magazine, shooter, speed));
+}
